a4e4978e566205abbb294e9817aff819c6786b19
niqnr <madenianaryantika@gmail.com>
2026-07-23 02:16:32 +0800

    Add vercel.json for rewrites and revert HashRouter to BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { auth } from './firebase'
 import { onAuthStateChanged } from 'firebase/auth'
 
